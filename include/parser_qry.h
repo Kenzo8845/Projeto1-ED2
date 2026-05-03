d1eefd620e0de08004ced9e369a79ed6ec3508c2
Kenai Silva Bazani <silvabazanikenai@gmail.com>
2026-05-03 20:47:59 -0300

    -

--- include/parser_qry.h
+++ include/parser_qry.h
@@ -5,6 +5,14 @@
 #include "hash.h"
 
 /**
+ * Este modulo lida com a leitura do arquivo qry.
+ * O arquivo qry é o principal arquivo de instruções do programa.
+ * Apos a leitura do geo e do pm, onde as quadras e os habitantes iniciais estão criados e cada um em seu hash,
+ * os comandos qry alteram, adicionam ou removem valores desses (habitantes ou quadras).
+ * Alguns comandos geram texto no arquivo txt do final da execução, e/ou imagnes no svg final. 
+ */
+
+/**
  * @brief Processa o arquivo .qry, executando comandos e gerando saídas.
  * @param caminho_arquivo Caminho do arquivo .qry
  * @param quadras Hash de Quadras
